dfa0cfa4ca0b65d88e514885a5ad8bf6cbb83ff8
librarywon <librarywon@naver.com>
2024-02-10 15:02:49 +0900

    1931_회의실배정 풀이 완료

---
+++ LCSA_study/src/main/kotlin/chapter3/1931_회의실배정.kt
@@ -0,0 +1,37 @@
+package chapter3
+
+var timeTable : MutableList<List<Int>> = mutableListOf()
+
+fun input() {
+    val n = readln().toInt()
+
+    // 리스트 입력 ex) [[1,3],[3,5]...]
+    for (i in 1..n) {
+        val mTime = readln().split(" ").map { it.toInt() }
+        timeTable.add(mTime)
+    }
+}
+fun main() {
+
+    // 예제 입력
+    input()
+
+    // 종료시간 기준으로 오름차순 정렬하고 종료시간이 같다면 시작시간 오름차순 정렬
+    val timeTable = timeTable.sortedWith(compareBy({ it[1] }, { it[0] }))
+
+    // 종료시간 초기값
+    var end = -1
+
+    // 갯수 Count
+    var count= 0
+
+    // 정렬된 시간
+    for (mTime in timeTable) {
+        if (end <= mTime[0]) {
+            end = mTime[1]
+            count++
+        }
+    }
+
+    println(count)
+}
