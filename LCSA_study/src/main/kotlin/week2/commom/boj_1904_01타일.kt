dfc784c65f7674500dd1c60ee74c0db14e70b6eb
librarywon <librarywon@naver.com>
2024-02-19 22:06:00 +0900

    boj_2512_예산 풀이

---
+++ LCSA_study/src/main/kotlin/week2/commom/boj_1904_01타일.kt
@@ -0,0 +1,19 @@
+package week2.commom
+
+// boj_1904_01타일
+
+fun main() {
+
+    val n = readln().toInt()
+
+    var memo = IntArray(1000001) { 0 }
+
+    memo[1] = 1
+    memo[2] = 2
+
+    for(i in 3..n) {
+        memo[i] = memo[i-1]%15746 + memo[i-2]%15746
+    }
+
+    println(memo[n]%15746)
+}
